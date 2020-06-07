e14ba82dc036e4a5b4b6f817a14ef9045afac22d
Stephanie Thompson <thompson.stephanie84@gmail.com>
2020-06-07 23:49:20 +0000

    Created basic CRUD functions. Needs more work

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pymongo
 import os
+import sys
 
 # Retrieve MongoDB Connection string from .env file
 mongo_connection = os.getenv("MONGO_CONNECTION")
@@ -9,36 +10,37 @@ client = pymongo.MongoClient(mongo_connection)
 
 # Set Variables for Database and Collection
 db = client['Portfolio']
-stocks_collection = db['Stocks']
 
 def main():
 
     while(1):
     # CRUD Option Selections
-        selection = input('\nSelect 1 to insert, 2 to update, 3 to read, 4 to delete\n')
+        selection = input('\nPlease make a selection from the following choices: \n 1: Create a new document\n 2: Select a document to view \n 3: Update a document\n 4: Delete a document \n 5: Quit \n')
     
         if selection == '1':
             insert()
         elif selection == '2':
-            update()
-        elif selection == '3':
             read()
+        elif selection == '3':
+            update()
         elif selection == '4':
             delete()
+        elif selection == '5':
+            quit()
         else:
-            print ('\n INVALID SELECTION \n')
+            print ('INVALID SELECTION \n')
 
 # Function to insert data into MongoDB
 def insert():
     try:
-        Ticker = raw_input('Enter Ticker Code :')
-        Profit_Margin = raw_input('Enter Profit Margin :')
-        Institutional_Ownership = raw_input('Enter Institutional Ownership :')
-        EPS_growth = raw_input('Enter EPS growth past 5 yearsCountry :')
-        Total_Debt_Equity = raw_input('Enter Total Debt/Equity :')
-        Current_Ratio = raw_input('Enter Current Ratio :')
+        Ticker = input('Enter Ticker Code :')
+        Profit_Margin = input('Enter Profit Margin :')
+        Institutional_Ownership = input('Enter Institutional Ownership :')
+        EPS_growth = input('Enter EPS growth past 5 yearsCountry :')
+        Total_Debt_Equity = input('Enter Total Debt/Equity :')
+        Current_Ratio = input('Enter Current Ratio :')
 
-        db.Employees.insert_one(
+        db.Stocks.insert_one(
             {
                 "Ticker":Ticker,
                 "Profit Margin":Profit_Margin,
@@ -47,7 +49,61 @@ def insert():
                 "Total Debt/Equity":Total_Debt_Equity,
                 "Current Ratio":Current_Ratio
         })
-        print ('\nInserted data successfully\n')
+        print ('Inserted data successfully\n')
     
     except Exception as e:
         print (e)
+
+#Function to read data from MongoDB
+def read():
+  ticker_selection = input('Enter the Ticker of the stock you would like to view: \n')
+  ticker_find = ({"Ticker": ticker_selection})
+  try:
+    stockColl = db.Stocks.find(ticker_find)
+    print ('Stocks')
+    for stock in stockColl:
+      print (stock)
+
+  except Exception as e:
+    print (e)
+
+#Function to update data in MongoDB
+def update():
+  try:
+    update_criteria = input('Enter Ticker of the Record you would like to update:\n')
+    update_profit = input('Enter New Profit Margin\n')
+
+    db.Stocks.update_one(
+      {"ticker": update_criteria},
+      {"$set": {
+        "Profit_Margin": update_profit
+       }
+      }
+    )
+
+    print ('Record updated successfully\n')
+
+  except Exception as e:
+    print (e)
+
+#Function to delete a record from MongoDB
+def delete():
+  try:
+    delete_criteria = input('Enter Ticker Name to delete: \n')
+    db.Stocks.delete_one({"Ticker":delete_criteria})
+    print ('Document Deleted')
+  except Exception as e:
+    print (e)
+
+#Function to quit program
+def quit():
+  try:
+    endProgram = input('Are you sure you want to quit? (Yes/No) ')
+    if endProgram == "Yes" or "yes":
+      sys.exit()
+    else: 
+      main()
+  except Exception as e:
+    print(e)
+ 
+main()
